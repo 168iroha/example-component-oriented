480788ce2261e24a5b4f91a1dd77866aadb2fcf7
168iroha <168iroha@168iroha.net>
2024-05-11 18:28:58 +0900

    useSSRDataで定数値を扱うことができるように修正 #16

--- sample/lib/useSSRData.js
+++ sample/lib/useSSRData.js
@@ -1,4 +1,4 @@
-import { CommonLabel, IState, Context, State, useState, watch } from "../../src/core.js";
+import { CommonLabel, IState, Context, State, Computed, NotState, useState, watch } from "../../src/core.js";
 
 /** onMountを即時評価するためのラベル */
 const onMountLabel = new CommonLabel();
@@ -7,21 +7,22 @@ const onMountLabel = new CommonLabel();
  * SSRで利用する状態変数の宣言
  * @template T, R
  * @param { Context } ctx 
- * @param { IState<unknown>[] | IState<unknown> } watchState callbackを再度呼びだすトリガーとなる状態変数
+ * @param { IState<unknown>[] | IState<unknown> | undefined } watchState callbackを再度呼びだすトリガーとなる状態変数
  * @param { () => Promise<T> } callback jsonを生成するコールバック
  * @param { R | undefined } init 状態変数の初期値
  * @param { (json: T) => R } transform jsonを状態変数として持つオブジェクトに変換をする関数
+ * @returns { IState<R | undefined> }
  */
 function useSSRData(ctx, watchState, callback, init, transform = x => x) {
 	const element = ctx.component?.element;
-	const state = useState(ctx, init);
 
 	const className = '__USE_SSR_DATA__';
 	const type = 'application/json';
 
 	const genStateNode = ctx.genStateNode;
 	if (ctx.waitFlag === 'nowait') {
-		const setState = () => callback().then(json => state.value = transform(json));
+		/** @type { (state: State<R | undefined>) => () => Promise<R> } */
+		const setState = state => () => callback().then(json => state.value = transform(json));
 		// 既に構築済みのノードに対して操作が行われる場合はデータの取り出しを試みる
 		if (element?.nodeType === ctx.window.Node.ELEMENT_NODE) {
 			const jsonElement = element.querySelector(`:scope > .${className}[type="${type}"]`);
@@ -30,19 +31,30 @@ function useSSRData(ctx, watchState, callback, init, transform = x => x) {
 				/** @type { T } */
 				const json = JSON.parse(jsonElement.textContent);
 				jsonElement.remove();
-				state.org = transform(json);
-				// watch
-				watch(ctx, watchState, setState);
-				return state;
+				const value = transform(json);
+				if (!(watchState instanceof State || watchState instanceof Computed)) {
+					// watchStateが状態変数でないときは状態をもたない変数を返す
+					return new NotState(value);
+				}
+				else {
+					const state = useState(ctx, value);
+					// watch
+					watch(ctx, watchState, setState(state));
+					return state;
+				}
 			}
 		}
-
-		// 取得済みのデータからデータの取得に失敗した場合はcallbackを実行してデータを取得する
-		setState();
-		// watch
-		watch(ctx, watchState, setState);
+		{
+			const state = useState(ctx, init);
+			// 取得済みのデータからデータの取得に失敗した場合はcallbackを実行してデータを取得する
+			setState(state)();
+			// watch
+			watchState && watch(ctx, watchState, setState(state));
+			return state;
+		}
 	}
 	else {
+		const state = useState(ctx, init);
 		if (genStateNode) {
 			/** @type { T | undefined } */
 			let json = undefined;
@@ -62,9 +74,8 @@ function useSSRData(ctx, watchState, callback, init, transform = x => x) {
 				}
 			}, onMountLabel);
 		}
+		return state;
 	}
-
-	return state;
 }
 
 export { useSSRData };
